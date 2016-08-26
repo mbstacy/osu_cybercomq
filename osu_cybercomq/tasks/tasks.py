c7606272901e986bdf10ff82c400bc02f61109d1
Mark Stacy <mbstacy@gmail.com>
2016-08-26 16:31:45 -0500

    updated q for mesonet

---
+++ osu_cybercomq/tasks/tasks.py
@@ -0,0 +1,32 @@
+from celery.task import task
+from dockertask import docker_task
+from subprocess import call,STDOUT
+import requests
+
+from datetime import datetime
+from okmesonet import weather
+from pymongo import MongoClient
+mesonetDB = "mesonet"
+appname = "osu_okmesonet"
+#Default base directory 
+#basedir="/data/static/"
+
+
+#Example task
+@task()
+def add(x, y):
+    """ Example task that adds two numbers or strings
+        args: x and y
+        return addition or concatination of strings
+    """
+    result = x + y
+    return result
+
+def get_mesonet_data(site,start_data,end_date):
+    mongo_uri ="mongodb://{0}:27017/".format(os.environ["{0}_MONGO_PORT_27017_TCP_ADDR".format(appname.upper())])
+    db=MongoClient(mongo_uri)
+    df=weather.get_mesonet_dataframe(datetime.strptime(start_date, '%Y-%m-%d'),datetime.strptime(end_date, '%Y-%m-%d'),site) 
+    data=df.T.to_dict().values()
+    result=db[mesonetDB][site].insert_many(data)
+    return "{0} records recorded for {1} mesonet site".format(len(result.inserted_ids),site)   
+
