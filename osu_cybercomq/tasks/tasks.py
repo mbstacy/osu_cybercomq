fa8c83d8cfb36ba6cdebc73c65d4cfecbab164b4
Mark Stacy <mbstacy@gmail.com>
2016-08-26 19:31:46 -0500

    insert

--- osu_cybercomq/tasks/tasks.py
+++ osu_cybercomq/tasks/tasks.py
@@ -29,6 +29,6 @@ def get_mesonet_data(site,start_date,end_date):
     db=MongoClient(mongo_uri)
     df=weather.get_mesonet_dataframe(datetime.strptime(start_date, '%Y-%m-%d'),datetime.strptime(end_date, '%Y-%m-%d'),site) 
     data=df.T.to_dict().values()
-    result=db[mesonetDB][site].insert_many(data)
+    result=db[mesonetDB][site].insert(data)
     return "{0} records recorded for {1} mesonet site".format(len(result.inserted_ids),site)   
 
